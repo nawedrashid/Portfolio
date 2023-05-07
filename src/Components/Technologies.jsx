5dc44cb94473b4f32374f27eccd492455cbe4927
Nawed Rashid <75127419+nawedrashid@users.noreply.github.com>
2023-05-07 21:09:55 +0530

    technologies overlapping fixed

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -133,7 +133,7 @@ const Technologies = () => {
       name="Technologies"
       className="bg-gradient-to-b from-grad2 to-grad1 w-full h-full sm:h-full pt-20"
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-screen h-full sm:h-screen text-white">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-screen h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4 border-gray p-2 inline">
             Technologies
